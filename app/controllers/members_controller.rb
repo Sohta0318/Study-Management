d708fcf9b0ad0a3dc4d7214d93309e43021aeee9
SOHTA0318 <nicosohta0318@gmail.com>
2022-02-17 03:31:16 -0800

    needs to be fixed

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,7 +2,20 @@ class MembersController < ApplicationController
   def index
   @members = User.all
   end
+  def create
+    friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: friend)
+    if current_user.save
+      flash[:notice] = 'You successfully followed!'
+      redirect_to members_path
+    else
+      flash[:alert] = 'Something went wrong'
+      redirect_to members_path
+    end
+  end
   def edit;end
-  def show;end
+  def show
+  @friends = current_user.friends
+  end
   def destroy;end
 end
